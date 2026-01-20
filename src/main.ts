ba35a5743bf434ca8741ed51937a8d0fb7c72efa
ak-ygt <arpita.kulkarni@ysecit.com>
2026-01-20 12:23:45 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ import {
 
 import { initDevtools } from '@pixi/devtools';
 import { Stats } from 'pixi-stats';
-import { Constants } from './constants';
+import { Constants } from './Constants';
 import { ParticleEffect } from './ParticleEffect';
 
 // ============================================================================
